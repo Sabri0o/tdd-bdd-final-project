779e2b2831df4d16e26493436f57b0fe70350798
TRABELSI Sabri <42933424+Sabri0o@users.noreply.github.com>
2024-08-14 15:28:02 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -109,6 +109,22 @@ def create_products():
 #
 # PLACE YOUR CODE HERE TO READ A PRODUCT
 #
+@app.route("/products/<product_id>", methods=["GET"])
+def get_products(product_id):
+    """
+    Get a Product
+    This endpoint will read a Product based ON the id passed in params
+    """
+    app.logger.info("Request to Retrieve a product with id [%s]", product_id)
+
+    found_product = Product.find(product_id)
+    
+    if not found_product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+ 
+    app.logger.info("Returning product: %s", found_product.name)
+    return found_product.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
